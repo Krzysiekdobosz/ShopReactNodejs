9a84c35c57b21c7845d70c285315cb5ca89b97a0
Krzysztof <krzysiekdobosz2001@gmail.com>
2024-07-08 21:57:15 +0200

    v4.5.1

--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -1,11 +1,23 @@
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
-export const LOGOUT = 'LOGOUT';
 export const GET_PRODUCTS = 'GET_PRODUCTS';
 export const GET_PRODUCT = 'GET_PRODUCT';
 export const ADD_PRODUCT = 'ADD_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
+
+export const GET_CART = 'GET_CART';
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
-export const GET_CART = 'GET_CART';
+
+export const GET_SAVED_ITEMS = 'GET_SAVED_ITEMS';
+export const ADD_TO_SAVED = 'ADD_TO_SAVED';
+export const REMOVE_FROM_SAVED = 'REMOVE_FROM_SAVED';
+
+
+export const USER_LOADED = 'USER_LOADED';
+export const USER_LOADING = 'USER_LOADING';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
